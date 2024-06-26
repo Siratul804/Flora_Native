33f1e6e9b65f2d6a6a36c5126de6d1901bc97271
Siratul Islam <siratulislam@Siratuls-Air.Dlink>
2024-06-26 17:12:16 +0600

    flora_bookamrk_remain

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,73 +1,86 @@
-import React from "react";
-import { Text, View, Image, FlatList } from "react-native";
+// Home.js
+import React, { useState } from "react";
+import { Text, View, Image, FlatList, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchInput from "../components/SearchInput";
+import ModalComponent from "../components/ModalComponent";
 
-import image1 from "../../assets/floras/2.jpg";
-import image2 from "../../assets/floras/3.jpg";
-import image3 from "../../assets/floras/4.jpg";
-import image4 from "../../assets/floras/5.jpg";
-import image5 from "../../assets/floras/6.jpg";
-import image6 from "../../assets/floras/7.jpg";
+const images = {
+  image1: require("../../assets/floras/2.jpg"),
+  image2: require("../../assets/floras/6.jpg"),
+  image3: require("../../assets/floras/5.jpg"),
+  image4: require("../../assets/floras/4.jpg"),
+  image5: require("../../assets/floras/3.jpg"),
+  image6: require("../../assets/floras/7.jpg"),
+};
 
 const data = [
   {
     prompt: "A Dreamy Black Cat",
     tags: ["Dreamy", "Night"],
     mark: "On",
-    image: image1,
+    image: images.image1,
   },
   {
     prompt: "Stained Angry Cat",
     tags: ["Stained"],
     mark: "On",
-    image: image5,
+    image: images.image2,
   },
   {
     prompt: "A Cute Cat",
     tags: ["Psycheelic"],
     mark: "On",
-    image: image4,
+    image: images.image3,
   },
-
   {
     prompt: "A Black Cat",
     tags: ["Dreamy", "Night"],
     mark: "Off",
-    image: image3,
+    image: images.image4,
   },
   {
     prompt: "A cat is sitting peacefully",
     tags: ["Dark", "Night"],
     mark: "Off",
-    image: image2,
+    image: images.image5,
   },
   {
     prompt: "Aggressive Angry Cat",
     tags: ["Retrowave"],
     mark: "Off",
-    image: image6,
+    image: images.image6,
   },
 ];
 
 const Home = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleImagePress = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+    console.log(selectedItem, modalVisible);
+  };
+
   const renderItem = ({ item }) => (
-    <View>
-      <View className="pt-4">
-        <Image
-          source={item.image}
-          className="rounded-2xl h-[25vh] w-[22vh]  "
-        />
-        <Text className="text-white text-[12px] text-center p-1 font-bold ">
-          {item.prompt}
-        </Text>
-        <Text className="inline-block text-center bg-[#212129] text-white text-[10px] font-medium py-1 px-3 rounded-2xl">
-          {" "}
-          #{item.tags.join("  #")}
-        </Text>
-        {/* <Text className="text-white">Mark: {item.mark}</Text> */}
+    <TouchableOpacity onPress={() => handleImagePress(item)}>
+      <View>
+        <View className="pt-4">
+          <Image
+            source={item.image}
+            className="rounded-2xl h-[25vh] w-[22vh]"
+          />
+          <Text className="text-white text-[12px] text-center p-1 font-bold ">
+            {item.prompt}
+          </Text>
+          <Text className="inline-block text-center bg-[#212129] text-white text-[10px] font-medium py-1 px-3 rounded-2xl">
+            {" "}
+            #{item.tags.join("  #")}
+          </Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 
   return (
@@ -89,6 +102,15 @@ const Home = () => {
           />
         </View>
       </View>
+      {selectedItem && (
+        <ModalComponent
+          isVisible={modalVisible}
+          onClose={() => setModalVisible(false)}
+          imageUri={Image.resolveAssetSource(selectedItem.image).uri}
+          prompt={selectedItem.prompt}
+          tags={selectedItem.tags}
+        />
+      )}
     </SafeAreaView>
   );
 };
---
+++ app/components/ModalComponent.jsx
@@ -0,0 +1,82 @@
+// components/ModalComponent.js
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import Modal from "react-native-modal";
+import * as FileSystem from "expo-file-system";
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+
+const ModalComponent = ({ isVisible, onClose, imageUri, prompt, tags }) => {
+  const downloadImage = async () => {
+    Alert.alert("Image Download Successfully !");
+    const fileUri = FileSystem.documentDirectory + imageUri.split("/").pop();
+    await FileSystem.downloadAsync(imageUri, fileUri)
+
+      .then(({ uri }) => {
+        console.log("Finished downloading to ", uri);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const bookMark = () => {
+    Alert.alert(" Image Added to Bookmark !");
+  };
+
+  return (
+    <Modal isVisible={isVisible} onBackdropPress={onClose}>
+      <View style={styles.modalContent}>
+        <View className="pb-4">
+          <Text className="text-white text-[12px] " style={styles.tags}>
+            Tags : #{tags.join("  #")}
+          </Text>
+        </View>
+        <Image source={{ uri: imageUri }} style={styles.image} />
+        <View className="pb-4">
+          <Text className="text-white text-[12px] font-bold">{prompt}</Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={downloadImage}
+          activeOpacity={0.7}
+          className="bg-orange-400 rounded-md h-[5vh] w-[26vh] flex items-center justify-center flex-row "
+        >
+          <Text className="font-bold pr-1 ">Download</Text>
+          <Feather name="download" size={20} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={bookMark}
+          activeOpacity={0.7}
+          className="bg-orange-400 rounded-md mt-2 h-[5vh] w-[26vh] flex items-center justify-center flex-row "
+        >
+          <Text className="font-bold pr-1 ">Bookmark</Text>
+          <Ionicons name="bookmark-outline" size={20} color="black" />
+        </TouchableOpacity>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContent: {
+    backgroundColor: "black",
+    padding: 60,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+});
+
+export default ModalComponent;
